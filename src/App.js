5466a42cd1157dfdefbe33bf8e3f538bd6c99015
MartinLord <martlord81@hotmail.com>
2020-04-17 11:56:44 +0100

    basic chart complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useEffect } from 'react';
 import './App.css';
+import { select, scaleLinear, max, scaleBand, axisLeft, axisBottom } from 'd3'
+import { data } from './data';
 
 function App() {
+
+  const svgRef = useRef();
+  const xValue =  d => d.population;
+  const yValue = d => d.country;
+  const margin = { top: 20, right: 20, bottom: 20, left: 70}
+
+
+  useEffect(() => {
+    const svg = select(svgRef.current);
+
+
+
+    const width = +(svg.attr('width'));
+    const height = +(svg.attr('height'));
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
+
+    // bar scale below---------------------------
+
+    const xScale = scaleLinear()
+      .domain([0, max(data, xValue)])
+      .range([0, innerWidth])
+
+    const yScale = scaleBand()
+      .domain(data.map(yValue))
+      .range([0, innerHeight])
+      .padding(0.2)
+
+
+      //svg below------------------------
+
+    const g = svg.append('g')
+      .attr('transform', `translate(${margin.left},${margin.top})`)
+
+    g.append('g').call(axisLeft(yScale))
+    g.append('g').call(axisBottom(xScale))
+    .attr('transform', `translate(0,${innerHeight})`)
+
+
+
+    g.style('background-color', 'purple')
+      .selectAll('rect')
+      .data(data)
+      .enter().append('rect')
+      .attr('y', d => yScale(yValue(d)))
+      .attr('width', d => xScale(xValue(d)))
+      .attr('height', yScale.bandwidth())
+      .attr('fill', 'steelblue')
+    
+    
+   }, [])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Making a Bar Chart in D3</h2>
+       <svg ref={svgRef} width="960" height="500">
+      </svg>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,13 @@
+const data = [
+    {country: "China", population: 1416257},
+    {country: "India", population: 1325998},
+    {country: "United States", population: 355627},
+    {country: "Pakistan", population: 300928},
+    {country: "Bangladesh", population: 166373},
+    {country: "Nigeria", population: 220934},
+    {country: "Indonesia", population: 289003},
+    {country: "Brazil", population: 198922},
+    {country: "Mexico", population: 134667},
+  ]
+
+  export { data };
